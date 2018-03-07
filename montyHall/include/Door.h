e30528a0448acd56398d30a76ac784b264f6097a
Ron Huff <ron.huff@ku.edu>
2018-03-07 08:45:20 -0600

    Create Door class. Declare member booleans.

---
+++ montyHall/include/Door.h
@@ -0,0 +1,14 @@
+#pragma once
+class Door
+{
+public:
+	Door();
+	~Door();
+
+
+private:
+	bool m_hasCar;
+	bool m_hasGoat;
+	bool m_isOpen;
+};
+
---
+++ montyHall/source/Door.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "..\include\Door.h"
+
+
+Door::Door()
+{
+	m_hasCar = false;
+	m_hasGoat = false;
+	m_isOpen = false;
+}
+
+
+Door::~Door()
+{
+}
